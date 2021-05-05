d20c3d176045926e586801106f7397875dc8a698
Maksim Rakitin <mrakitin@bnl.gov>
2021-05-04 23:52:02 -0400

    Add docker_image and script_name to params

--- render.py
+++ render.py
@@ -3,6 +3,8 @@ import os
 from jinja2 import Template
 
 params = {
+    # "docker_image": "nsls2/debian-with-miniconda:v0.1.2",
+    "docker_image": "quay.io/condaforge/linux-anvil-comp7:latest",
     "env_name": "bmm-analysis",
     "python_version": "3.9",
     "pkg_name": "",
@@ -13,7 +15,8 @@ params = {
 
 template_file = "gen-conda-packed-env.sh.j2"
 name = os.path.splitext(os.path.splitext(template_file)[0])
-outfile = f"{name[0]}-{params['env_name']}{name[1]}"
+script_name = f"{name[0]}-{params['env_name']}{name[1]}"
+params["script_name"] = script_name
 
 with open(template_file) as f:
     template = Template(f.read())
@@ -21,5 +24,5 @@ with open(template_file) as f:
 text = template.render(**params)
 print(text)
 
-with open(outfile, "w") as f:
+with open(script_name, "w") as f:
     f.write(text)
