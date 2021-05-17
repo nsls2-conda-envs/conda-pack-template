12bccbbec15c3ae0dd0cee05a9bbc0f80e01cd0b
Maksim Rakitin <mrakitin@bnl.gov>
2021-05-17 17:10:00 -0400

    Extract the parameters in a yaml config file

--- render.py
+++ render.py
@@ -1,30 +1,45 @@
+import argparse
 import os
 
+import yaml
 from jinja2 import Template
 
-params = {
-    # "docker_image": "nsls2/debian-with-miniconda:v0.1.2",
-    "docker_image": "quay.io/condaforge/linux-anvil-comp7:latest",
-    "env_name": "astra-toolbox",
-    "python_version": "3.8",
-    "pkg_name": "",
-    "pkg_version": "",
-    "extra_packages": "astra-toolbox tomopy",
-    "channels": "-c astra-toolbox/label/dev -c conda-forge",
-}
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=(
+            "Render a generation script based on the specified config"
+            "file to produce a conda-pack'd environment."
+        )
+    )
 
-script_location = os.path.abspath(os.path.dirname(__file__))
-print(f"Script location: {script_location}")
-template_file = os.path.join(script_location, "gen-conda-packed-env.sh.j2")
-name = os.path.splitext(os.path.splitext(os.path.basename(template_file))[0])
-script_name = f"{name[0]}-{params['env_name']}{name[1]}"
-params["script_name"] = script_name
+    parser.add_argument(
+        "-c", "--config-file", dest="config_file", help="the input config file"
+    )
+    args = parser.parse_args()
 
-with open(template_file) as f:
-    template = Template(f.read())
+    config_file = os.path.abspath(args.config_file)
 
-text = template.render(**params)
-print(text)
+    if not os.path.isfile(config_file):
+        raise RuntimeError(f"The config file '{config_file}' is not found.")
 
-with open(script_name, "w") as f:
-    f.write(text)
+    with open(config_file) as f:
+        params = yaml.load(f, Loader=yaml.SafeLoader)
+
+    script_location = os.path.abspath(os.path.dirname(__file__))
+    print(f"Script location: {script_location}")
+    template_file = os.path.join(script_location, "gen-conda-packed-env.sh.j2")
+    # Get the base name, i.e. 'gen-conda-packed-env.sh':
+    name = os.path.splitext(os.path.basename(template_file))[0]
+    # Split the base name into ('gen-conda-packed-env', '.sh'):
+    name = os.path.splitext(name)
+    script_name = f"{name[0]}-{params['env_name']}{name[1]}"
+    params["script_name"] = script_name
+
+    with open(template_file) as f:
+        template = Template(f.read())
+
+    text = template.render(**params)
+    print(text)
+
+    with open(script_name, "w") as f:
+        f.write(text)
