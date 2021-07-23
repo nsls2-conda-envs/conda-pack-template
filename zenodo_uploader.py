807bfd83506bf8818b126d9d508d25849a0c8c8e
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-23 15:27:14 -0400

    Update zenodo_uploader.py

--- zenodo_uploader.py
+++ zenodo_uploader.py
@@ -1,5 +1,6 @@
 import requests
-ACCESS_TOKEN = input("Enter Access Token: ")
+import os
+os.getenv('ZENODO_ACCESS_TOKEN: ')
 headers = {"Content-Type": "application/json"}
 params = {'access_token': ACCESS_TOKEN}
 r = requests.post('https://sandbox.zenodo.org/api/deposit/depositions', params=params, json={}, headers=headers)
@@ -9,7 +10,7 @@ path = input("Enter file path as '/path/to/my-file.zip': ")
 print("Hold on. This will take a while...")
 with open(path, "rb") as fp:
     r = requests.put(
-        "%s/%s" % (bucket_url, filename),
+        "f"(bucket_url, filename),
         data=fp,
         params=params
     )
