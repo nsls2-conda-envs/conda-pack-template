cf639cdcb40d4d3836d60090176716f21564d652
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-27 11:52:44 -0400

    Update export.sh

--- export.sh
+++ export.sh
@@ -6,7 +6,6 @@ echo "Environment name: ${env_name}"
 echo "Channels: ${channels}"
 
 conda activate ${env_name}
-#while read requirement;do conda install --yes ${channels} --override-channels ${requirement};done < /requirements.txt
 conda env export -n ${env_name} -f ${env_name}.yml ${channels} --override-channels
 conda-pack -o ${env_name}.tar.gz
 openssl sha256 ${env_name}.tar.gz > ${env_name}-sha256sum.txt
