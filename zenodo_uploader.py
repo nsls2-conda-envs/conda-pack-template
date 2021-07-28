2e09b7a9629cf8e382aa03057c043b3df89d8d14
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-28 17:54:53 -0400

    Update zenodo_uploader.py

--- zenodo_uploader.py
+++ zenodo_uploader.py
@@ -1,25 +1,48 @@
 import os
-from pathlib import Path
-import yaml
-from pyzenodo3.upload import upload
-import argparse
 
-def upload_to_zenodo(file_name_to_upload, base_url="https://sandbox.zenodo.org/api/"):
-    upload(
-        datafn=Path(file_name_to_upload),
-        token=os.getenv("ZENODO_ACCESS_TOKEN", ""),
-        metafn="",
+import argparse
+import requests
+import traceback
+
+
+def upload_to_zenodo(file_name_to_upload, zenodo_server="https://sandbox.zenodo.org/api/deposit/depositions"):
+    headers = {"Content-Type": "application/json"}
+    params = {"access_token": os.getenv("ZENODO_ACCESS_TOKEN", "")}
+    r = requests.post(
+        zenodo_server,
+        params=params,
+        json={},
+        headers=headers,
     )
+    deposition_id = r.json()["record_id"]
+    #print(r.status_code)
+    #print(r.json())
+    bucket_url = r.json()["links"]["bucket"]
+    filename = file_name_to_upload
+    print("\nHold on. This will take a while...")
+    with open(filename, "rb") as fp:
+        try:
+            r = requests.put(f"{bucket_url}/{filename}", data=fp, params=params)
+            print(
+                f"\nFile Uploaded successfully!\nFile link: {r.json()['links']['self']}"
+            )
+            print(r.status_code)
+            print(r.json())
+        except Exception as excinfo:
+            print(
+                f"\nFailed to upload file! Here is why:\n{''.join(traceback.format_exception(None, excinfo, excinfo.__traceback__))}"
+            )
+    r = requests.post(f"{zenodo_server}/{deposition_id}/actions/publish", params=params)
+    #print(r.status_code)
+    #print(r.json())
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description=(
-            "Upload files to Zenodo."
-        )
-    )
+    parser = argparse.ArgumentParser(description=("Upload files to Zenodo."))
+
     parser.add_argument(
-        "-f", "--file to upload", dest="file_name_to_upload", help="the input config file"
+        "-f", "--file", dest="file_name_to_upload", help="the input config file"
     )
     args = parser.parse_args()
     upload_to_zenodo(args.file_name_to_upload)
-    print("Upload Done!")
+
