58a2dfc626c8c1e4a343ad4b720f20c8a5ee86c6
johngedell <74158598+johngedell@users.noreply.github.com>
2021-08-02 15:06:25 -0400

    Added Search Function, Publish Option, Multiple File Uploads, Zenodo Token Requirement

--- zenodo_uploader.py
+++ zenodo_uploader.py
@@ -4,78 +4,235 @@ import argparse
 import requests
 import yaml
 import json
+from urllib.parse import urlencode
+
+
+def search_for_deposition(
+    title,
+    owner=None,
+    creators=None,
+    zenodo_server="https://sandbox.zenodo.org/api/",
+):
+    print(f"Searching for depositions...\n")
+    search = f'metadata.title:"{title}"'
+    if owner:
+        search += f" owners:{owner}"
+    if creators:
+        creators_query = '"' + '" OR "'.join(creators) + '"'
+        search += f" metadata.creators.name:{creators_query}"
+    search = search.replace("/", " ")  # zenodo can't handle '/' in search query
+
+    params = {"q": search, "sort": "bestmatch"}
+    url = zenodo_server + "records?" + urlencode(params)
+
+    records = [hit for hit in requests.get(url).json()["hits"]["hits"]]
+
+    if not records:
+        print(f"No records found for search: '{title}'")
+        return None, None, None
+
+    print(f"Found `{len(records)}` depositions!")
+
+    deposition = records[0]
+    print(f"Best match is deposition: {deposition['id']}")
+    return (
+        deposition["id"],
+        deposition["links"]["bucket"],
+        deposition["links"]["html"].replace("deposit", "record"),
+    )
+
+
+def create_new_version(
+    deposition_id, token, zenodo_server="https://sandbox.zenodo.org/api/"
+):
+    url = f"{zenodo_server}deposit/depositions/{deposition_id}/actions/newversion"
+    r = requests.post(
+        url,
+        params={"access_token": token},
+    )
+    r.raise_for_status()
+
+    deposition = r.json()
+    new_deposition_url = deposition["links"]["latest_draft"]
+    new_deposition_id = new_deposition_url.split("/")[-1]
+
+    r = requests.get(
+        f"{zenodo_server}deposit/depositions/{new_deposition_id}",
+        params={"access_token": token},
+    )
+    r.raise_for_status()
+    deposition = r.json()
+
+    return (
+        deposition["id"],
+        deposition["links"]["bucket"],
+        deposition["links"]["html"].replace("deposit", "record"),
+    )
+
+
+def create_new_deposition(token, zenodo_server="https://sandbox.zenodo.org/api/"):
+    url = f"{zenodo_server}deposit/depositions"
+    r = requests.post(
+        url,
+        params={"access_token": token},
+        json={},
+        headers={"Content-Type": "application/json"},
+    )
+    r.raise_for_status()
+
+    deposition = r.json()
+
+    return (
+        deposition["id"],
+        deposition["links"]["bucket"],
+        deposition["links"]["html"].replace("deposit", "record"),
+    )
+
 
 def upload_to_zenodo(
-    file_name_to_upload,
-    config_file,
-    zenodo_server="https://sandbox.zenodo.org/api/deposit/depositions",
+    filename,
+    bucket_url,
+    file_url,
+    filebase,
+    token,
 ):
-    filename = os.path.abspath(file_name_to_upload)
-    if not os.path.isfile(filename):
-        raise FileNotFoundError(
-            f"The file, specified for uploading does not exist: {filename}"
-        )
-        
-    config_name = os.path.abspath(config_file)
-    if not os.path.isfile(config_name):
-        raise FileNotFoundError(
-            f"The file with metadata, specified for uploading does not exist: {config_name}"
-        )
-        
-    headers = {"Content-Type": "application/json"}
-    params = {"access_token": os.getenv("ZENODO_ACCESS_TOKEN", "")}
-    r = requests.post(
-        zenodo_server,
-        params=params,
-        json={},
-        headers=headers,
-    )
-    if r.status_code != 201:
-        raise RuntimeError(f"The status code for the request is {r.status_code}.\nMessage: {r.text}")
-
-    return_json = r.json()
-    deposition_id = return_json["id"]
-    bucket_url = return_json["links"]["bucket"]
-
-    filebase = os.path.basename(file_name_to_upload)
-
-    file_url = return_json["links"]["html"].replace('deposit', 'record')
 
     print(f"Uploading {filename} to Zenodo. This may take some time...")
     with open(filename, "rb") as fp:
-        r = requests.put(f"{bucket_url}/{filebase}", data=fp, params=params)
-        if r.status_code != 200:
-            raise RuntimeError(f"The status code for the request is {r.status_code}.\nMessage: {r.text}")
+        r = requests.put(
+            f"{bucket_url}/{filebase}", data=fp, params={"access_token": token}
+        )
+
+        r.raise_for_status()
+
         print(f"\nFile Uploaded successfully!\nFile link: {file_url}")
 
+
+def add_meta_data(
+    deposition_id,
+    meta_data,
+    token,
+    zenodo_server="https://sandbox.zenodo.org/api/",
+):
     print(f"Uploading metadata for {filename} ...")
-    with open(config_file) as fp:
 
-        data = yaml.safe_load(fp)
+    r = requests.put(
+        f"{zenodo_server}deposit/depositions/{deposition_id}",
+        params={"access_token": token},
+        data=json.dumps(meta_data),
+        headers={"Content-Type": "application/json"},
+    )
 
-        r = requests.put(
-            f"{zenodo_server}/{deposition_id}",
-            params=params,
-            data=json.dumps(data["zenodo_metadata"]),
-            headers=headers,
-        )
-        if r.status_code != 200:
-            raise RuntimeError(f"The status code for the request is {r.status_code}.\nMessage: {r.text}")
+    r.raise_for_status()
 
+
+def publish_file(
+    deposition_id,
+    filebase,
+    token,
+    zenodo_server="https://sandbox.zenodo.org/api/",
+):
     print(f"Publishing {filebase}...")
-    r = requests.post(f"{zenodo_server}/{deposition_id}/actions/publish", params=params)
-    if r.status_code != 202:
-        raise RuntimeError(f"The status code for the request is {r.status_code}.\nMessage: {r.text}")
+    r = requests.post(
+        f"{zenodo_server}/{deposition_id}/actions/publish",
+        params={"access_token": token},
+    )
+
+    r.raise_for_status()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=("Upload files to Zenodo."))
     parser.add_argument(
-        "-f", "--file", dest="file_name_to_upload", help="path to the file to be uploaded",
+        "-f",
+        "--file",
+        dest="files_to_upload",
+        help="path to the file to be uploaded",
+        required=True,
+        action="append",
     )
     parser.add_argument(
-        "-c", "--config-file", dest="config_file", help="config file with metadata information"
+        "-c",
+        "--config-file",
+        dest="config_file",
+        help="config file with metadata information",
+        required=True,
     )
-    
+    parser.add_argument(
+        "-p",
+        "--publish",
+        dest="publish",
+        action="store_true",
+        help="Whether to publish file or not",
+    )
     args = parser.parse_args()
-    upload_to_zenodo(args.file_name_to_upload, args.config_file)
+
+    token = os.getenv("ZENODO_ACCESS_TOKEN")
+    if not token:
+        exit(
+            "No access token provided!\n"
+            "Please create an environment variable with the token.\n"
+            "Variable Name: `ZENODO_ACCESS_TOKEN`"
+        )
+
+    config_file = os.path.abspath(args.config_file)
+    if not os.path.isfile(config_file):
+        raise FileNotFoundError(
+            f"The file with metadata, specified for uploading does not exist: {config_file}"
+        )
+
+    with open(config_file) as fp:
+
+        try:
+            meta_data = yaml.safe_load(fp)["zenodo_metadata"]
+        except:
+            exit(f"Please add metadata to the config file: {config_file}")
+
+    for file in args.files_to_upload:
+        filename = os.path.abspath(file)
+        filebase = os.path.basename(filename)
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(
+                f"The file, specified for uploading does not exist or is a directory: {filename}"
+            )
+
+        deposition_id, bucket_url, file_url = search_for_deposition(
+            title=meta_data["metadata"]["title"],
+            creators=(creator["name"] for creator in meta_data["metadata"]["creators"]),
+        )
+
+        if not deposition_id:
+            deposition_id, bucket_url, file_url = create_new_deposition(token=token)
+            upload_to_zenodo(
+                filename=file,
+                bucket_url=bucket_url,
+                file_url=file_url,
+                filebase=filebase,
+                token=token,
+            )
+            add_meta_data(
+                deposition_id=deposition_id,
+                meta_data=meta_data,
+                token=token,
+            )
+
+            if args.publish:
+                publish_file(
+                    deposition_id=deposition_id, filebase=filebase, token=token
+                )
+        else:
+            deposition_id, bucket_url, file_url = create_new_version(
+                deposition_id=deposition_id,
+                token=token,
+            )
+            upload_to_zenodo(
+                filename=file,
+                bucket_url=bucket_url,
+                file_url=file_url,
+                filebase=filebase,
+                token=token,
+            )
+            if args.publish:
+                publish_file(
+                    deposition_id=deposition_id, filebase=filebase, token=token
+                )
