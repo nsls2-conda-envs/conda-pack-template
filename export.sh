f44fcca8cbd305bac3a503f5172832a60986e535
Maksim Rakitin <mrakitin@bnl.gov>
2021-11-29 12:58:58 -0500

    Use `-n <env_name>` for `conda create` commands

--- export.sh
+++ export.sh
@@ -12,8 +12,8 @@ echo "Build dir       : ${build_dir}"
 
 source /opt/conda/etc/profile.d/conda.sh
 
-conda activate /opt/conda_envs/${env_name}
-conda env export -p /opt/conda_envs/${env_name} -f ${build_dir}/${env_name}.yml ${channels} --override-channels
+conda activate ${env_name}
+conda env export -n ${env_name} -f ${build_dir}/${env_name}.yml ${channels} --override-channels
 conda-pack -o ${build_dir}/${env_name}.tar.gz
 openssl sha256 ${build_dir}/${env_name}.tar.gz > ${build_dir}/${env_name}-sha256sum.txt
 openssl md5 ${build_dir}/${env_name}.tar.gz > ${build_dir}/${env_name}-md5sum.txt
