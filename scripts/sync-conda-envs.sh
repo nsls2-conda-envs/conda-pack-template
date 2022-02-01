7c47a1c8fa6c1e897f502dd8e230c63b1859b860
Maksim Rakitin <mrakitin@bnl.gov>
2022-01-31 22:18:56 -0500

    Add conda sync script/crontab config

---
+++ scripts/sync-conda-envs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+staged_envs_path="/conda-envs-staging"
+conda_envs_path="/nsls2/repos/conda/environments"
+
+owner="nsls2data"
+group="nsls2software"
+
+allowed_extensions=(".tar.gz" ".yml" "md5sum.txt" "sha256sum.txt")
+
+for ext in ${allowed_extensions[@]}; do
+    new_files_exist=$(ls -1 ${staged_envs_path}/*${ext} 2>/dev/null)
+    if [ ! -z "${new_files_exist}" ]; then
+        echo -e "Files with extension '${ext}' exist:\n\n${new_files_exist}\n"
+        cp -v ${staged_envs_path}/*${ext} ${conda_envs_path}/
+        chown -v ${owner}:${group} ${conda_envs_path}/*
+        chmod -v 664 ${conda_envs_path}/*
+    else
+        echo -e "NO files with extension '${ext}' exist:\n\n${new_files_exist}\n"
+    fi
+    unset new_files_exist
+done
+
+exit 0
