df2cde3ca0d278e845cd4d80a0f238a0e3828104
Maksim Rakitin <mrakitin@bnl.gov>
2022-04-12 12:56:03 -0400

    Add CLI args for the `conda-pack` command

--- export.sh
+++ export.sh
@@ -14,7 +14,8 @@ source /opt/conda/etc/profile.d/conda.sh
 
 conda activate ${env_name}
 conda env export -n ${env_name} -f ${build_dir}/${env_name}.yml ${channels} --override-channels
-conda-pack -o ${build_dir}/${env_name}.tar.gz
+# Per https://conda.github.io/conda-pack/cli.html:
+conda-pack -o ${build_dir}/${env_name}.tar.gz --ignore-missing-files --ignore-editable-packages
 openssl sha256 ${build_dir}/${env_name}.tar.gz > ${build_dir}/${env_name}-sha256sum.txt
 openssl md5 ${build_dir}/${env_name}.tar.gz > ${build_dir}/${env_name}-md5sum.txt
 chmod -v 664 ${build_dir}/${env_name}[.-]*
